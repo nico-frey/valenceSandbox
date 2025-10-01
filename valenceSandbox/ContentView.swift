5f7392f5c04da490149756da62e12248c20c8b70
nico-frey <nico@freyy.ch>
2025-10-01 11:15:46 +0200

    Initial Commit

---
+++ valenceSandbox/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  valenceSandbox
+//
+//  Created by Nico Frey on 01.10.2025.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ valenceSandbox/valenceSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  valenceSandboxApp.swift
+//  valenceSandbox
+//
+//  Created by Nico Frey on 01.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct valenceSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
